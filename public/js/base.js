0ef8871e1e36332d1fcf9b054afd2d024a1415c5
Demo Enviroment <kouya114@outlook.jp>
2025-08-04 18:53:35 +0900

    編集

--- public/js/base.js
+++ public/js/base.js
@@ -7,3 +7,13 @@ document.addEventListener("DOMContentLoaded", function() {
         menu.classList.toggle("active");
     });
 });
+
+// スライドショー
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}, 5000);
